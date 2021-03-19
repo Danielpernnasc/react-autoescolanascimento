6a3c727e28b5f952a8f1334d6fe55a2afece96a7
Daniel <dpericles6@gmail.com>
2021-03-18 23:40:29 -0300

    nova atualizacao

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ThemeProvider from "./styles/ThemeProvider";
 import GlobaSytle from "./styles/GlobalStyle";
 
 import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
 function App() {
   return (
     <ThemeProvider>
       <GlobaSytle />
-      <Home />
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path="/Sobre" element={<About/>} />
+        </Routes>
+      </Router>
     </ThemeProvider>
   );
 }
