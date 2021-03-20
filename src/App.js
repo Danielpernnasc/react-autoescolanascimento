71e1de6ada17379c46bc7598353078b0f81ca21e
Daniel <dpericles6@gmail.com>
2021-03-20 17:41:07 -0300

    commit app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React from "react";
+
+import  { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import ThemeProvider from "./styles/ThemeProvider";
 import GlobaSytle from "./styles/GlobalStyle";
 
 import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+// import Product from './components/pages/ProductDetail';
 
 function App() {
   return (
     <ThemeProvider>
       <GlobaSytle />
-      <Home />
+         <Router>
+           <Routes>
+             <Route path="/" element={<Home/>}/>
+             <Route path="/Sobre" element={<About/>}/>
+           </Routes>
+         </Router>
     </ThemeProvider>
   );
 }
