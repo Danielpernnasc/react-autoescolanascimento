33c9a7793ad0a083adb1edb00e0916bd2744c1ae
Daniel <dpericles6@gmail.com>
2021-03-22 18:44:56 -0300

    fix test-utils

--- src/components/test-utils.js
+++ src/components/test-utils.js
@@ -1,15 +1,16 @@
 import React from "react";
 import { render } from "@testing-library/react";
+import { MemoryRouter as Router } from "react-router-dom";
 
 
 import ThemeProvider from "../styles/ThemeProvider";
 
 
 const TestProvider = ({ children }) => (
-
+  <Router>      
     <ThemeProvider>{children}</ThemeProvider>
- 
-);
+  </Router>
+ );
 
 export * from "@testing-library/react";
 
