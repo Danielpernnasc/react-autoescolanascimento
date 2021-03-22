b18ad797971175fb93b8b5d15a2557a607bf14cb
Daniel <dpericles6@gmail.com>
2021-03-22 18:51:38 -0300

    atualizacao test

--- src/components/pages/ProductDetail.test.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import { render, screen } from "../test-utils";
-import ProductDetail from './ProductDetail';
-
-test('renders ProductDetail page', () => {
-  render(<ProductDetail />);
-  expect (screen.getByText('Nossos Serviços')).toBeInTheDocument();
-});
